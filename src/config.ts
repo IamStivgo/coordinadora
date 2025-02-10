7af021117d26594409ac7bd17b63de3311f7b3c3
iamstivgo <iamstivgo@gmail.com>
2025-02-10 00:51:01 -0500

    Update configuration add database  parameters

--- src/config.ts
+++ src/config.ts
@@ -3,10 +3,14 @@ dotenv.config()
 
 interface ENV {
   NODE_ENV: string | undefined
-  PORT: number
-  CLIENT_URL: string
-  MYSQL_URL: string | undefined
-  JWT_SECRET: string | undefined
+  PORT: number | undefined
+  CLIENT_URL: string | undefined
+  DB_HOST: string | undefined
+  DB_USER: string | undefined
+  DB_PASSWORD: string | undefined
+  DB_NAME: string | undefined
+  DB_PORT: number | undefined
+  JWT_SECRET: string
   CACHE_DB_PORT: number | undefined
   CACHE_DB_HOST: string | undefined
   CACHE_DB_PASSWORD: string | undefined
@@ -17,8 +21,12 @@ const getConfig = (): ENV => {
     NODE_ENV: process.env.NODE_ENV,
     PORT: process.env.PORT ? Number(process.env.PORT) : 3000,
     CLIENT_URL: process.env.CLIENT_URL || '*',
-    MYSQL_URL: process.env.MYSQL_URL,
-    JWT_SECRET: process.env.JWT_SECRET,
+    DB_HOST: process.env.DB_HOST,
+    DB_USER: process.env.DB_USER,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+    DB_NAME: process.env.DB_NAME,
+    DB_PORT: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
+    JWT_SECRET: process.env.JWT_SECRET || 'secret',
     CACHE_DB_PORT: process.env.CACHE_DB_PORT
       ? Number(process.env.CACHE_DB_PORT)
       : undefined,

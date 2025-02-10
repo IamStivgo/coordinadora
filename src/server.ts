26aaad17d1b84817d6944a048ef382a2c57197f4
iamstivgo <iamstivgo@gmail.com>
2025-02-09 23:19:37 -0500

    Add routing for user authentication

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,17 @@
 import express from 'express';
-const app = express();
-app.use(express.json());
+import cors from 'cors';
+import config from './config';
+import router from './adapters/routes';
 
-const PORT = 3000;
+const PORT = config.PORT;
+const CLIENT_URL = config.CLIENT_URL;
+
+const app = express();
+
+app.use(express.json());
+app.use(cors({ origin: CLIENT_URL }));
+app.use(router);
+
 
 app.get('/status', (_req, res) => {
 	res.send({
